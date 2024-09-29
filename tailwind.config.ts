f54f7eb3ec20f796e5b04ffcd4c45e55b3691e78
SirMarkus73 <marcosbarreroherrero@gmail.com>
2024-09-30 00:43:11 +0200

    remove useless comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,6 @@ import prose from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
