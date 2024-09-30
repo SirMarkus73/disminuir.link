5ac6a42e1cd39201c2afe5ffdbd826b901c3a320
SirMarkus73 <marcosbarreroherrero@gmail.com>
2024-09-30 11:07:26 +0200

    get hostName

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,9 +6,13 @@ import {
   Input,
   Snippet,
 } from '@nextui-org/react'
+import { headers } from 'next/headers'
 import { handleSubmit } from './actions'
 
 function Dashboard() {
+  const headersList = headers()
+  const hostname = headersList.get('x-forwarded-host')
+
   return (
     <main className="flex flex-col justify-center items-center gap-3 p-3">
       <form action={handleSubmit} className="w-full md:w-3/4 grid gap-2">
@@ -29,7 +33,7 @@ function Dashboard() {
         <CardHeader>Ultimo enlace acortado</CardHeader>
         <CardBody>
           <Snippet variant="shadow" color="primary">
-            <a href="/dashboard">disminuir.link</a>
+            <a href="/dashboard">{`${hostname}/A4`} </a>
           </Snippet>
         </CardBody>
       </Card>
